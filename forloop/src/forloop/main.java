5082dbf8c600c3285da799a5af3e1b6a03b88b70
ELCOT <ELCOT@192.168.137.159>
2022-11-02 03:36:34 -0700

    first commit

---
+++ forloop/src/forloop/main.java
@@ -0,0 +1,16 @@
+package forloop;
+
+public class main {
+
+	public static void main(String[] args) {
+	int n = 5;
+	for(int i=1; i<=n; i++)
+		System.out.println("java is fun");
+	}
+}
+
+	
+
+	
+
+
